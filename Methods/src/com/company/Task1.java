cca2fec56a2c7719a17d9cf7d70798e9f8d1aa2a
pavelhan <polischuk.p.l@gmail.com>
2017-10-02 21:15:21 +0300

    Done first1

--- Methods/src/com/company/Task1.java
+++ Methods/src/com/company/Task1.java
@@ -3,6 +3,17 @@ package com.company;
 public class Task1 {
 
     public static void main(String[] args) {
-        //
+        //1. Создать метод вывода на консоль погодных условий. В метод передаётся температура (в градусах Цельсия)
+        // и скорость ветра. На консоль выдаётся строки типа: «Холодно, небольшой ветер», «Тепло, безветренно» и т.п.
+
+        weather(6, 15);
+    }
+
+    public static void weather(int temperature, int windSpeed){
+        if(temperature >= 15 & windSpeed <=5){
+            System.out.println("Тепло, безветрянно");
+        }else {
+            System.out.println("Холодно, небольшой ветер");
+        }
     }
 }

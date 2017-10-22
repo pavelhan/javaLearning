a8784ff58ac312ed1be51cefe6cb4df0b970a215
pavel.polischuk <pavelhan89@ukr.net>
2017-10-22 23:50:48 +0300

    added comments

--- OOP/src/com/company/devicesHierarhy/Device.java
+++ OOP/src/com/company/devicesHierarhy/Device.java
@@ -17,41 +17,44 @@ public class Device {
     Device pluggedDevice;
     PowerGenerator currentGenerator;
 
-
+    //Unique ID generator
     private void setDeviceID() {
         deviceID = guid;
         guid++;
     }
-
+    //Constructor 1
     public Device(String name, int power) {
         this.name = name;
         this.power = power;
         setDeviceID();
     }
-
+    //Constructor 2
     public Device(String name) {
         this.name = name;
         setDeviceID();
     }
 
+    //Getter for DeviceID
     public int getDeviceID() {
         return deviceID;
     }
 
+    //Getter for DeviceName
     public String getName() {
         return name;
     }
 
+    //Setter for DeviceName
     public void setName(String name) {
         this.name = name;
     }
 
-
+    //Getter for Device power
     public int getPower() {
         return power;
     }
 
-
+    //Setter for Device power
     public Device setPower(int power) {
 
         this.power = power;
@@ -92,6 +95,8 @@ public class Device {
         количеству оперативной памяти, телевизоров – по размеру диагонали, остальных устройств – по потребляемой мощности.
         */
 
+
+    //Compare devices method
     public void compare(Device device2) {
         String s1 = "1st device is bigger than second";
         String s2 = "2nd device is bigger than first";
@@ -119,25 +124,36 @@ public class Device {
     }
 
 
-
+    //Connect devices
     public Device connectDevice(Device device){
+        //if instance which call this method is instance of PowerGenerator class than we initialize currentGenerator variable with current object
         if (this instanceof PowerGenerator){
             device.currentGenerator = (PowerGenerator) this;
+        // if this is not power generator object than we can assume that this instanse already has some generator and we can use the same one for other devices
         }else {
             device.currentGenerator = this.currentGenerator;
         }
+        // here we check if current generator has enough of total power left to connect current device; also there is a check that if we don't have pluggedDevice to current device than we can connect some
         if (device.power <= device.currentGenerator.getTotalPower() & this.pluggedDevice == null){
+            //if the one who call this method don't have connected device yet, than we can connect some, and assign current device from parameter to pluggedDevice
             this.pluggedDevice = device;
+            //if there is enough totalPower left in power generator we can connect device and decrease it totalPower
             device.currentGenerator.changeTotalPower(device);
         }else {
+            //if there is not enough power than we write to console that device can't be connected
             System.out.println("Device " + device.name + "can't be connected");
+            //and just return device back
             return this;
         }
+        //if device connected successfully we also return it back
         return device;
     }
 
+    //This method is building device chain
     private String deviceChain(Device device){
+        // creation of chain string which will concatenate all connected devices using recursion
         String chain;
+        //we will call this one until there will be no connected device left
         if (device.pluggedDevice ==null){
             chain = device.name;
             return chain;
@@ -146,10 +162,12 @@ public class Device {
         return chain;
     }
 
+    //print device chain using method above
     public void printDeviceChain(){
         System.out.println(deviceChain(this));
     }
 
+    // this method changing current plugged device to Null and in this way brake a chain
     public void unplugDevice(){
         this.pluggedDevice = null;
     }
